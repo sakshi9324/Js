965eff0794d68353770668705c34a87ad7abd168
sakshi9324 <156990357+sakshi9324@users.noreply.github.com>
2024-12-16 07:00:42 +0000

    variables understanding in Js

---
+++ o1basics_Js/01_variables.js
@@ -0,0 +1,17 @@
+const emailId= "singsakshi@google.com"
+let accountId= "123456"
+var accountCity = "Lucknow"
+accountpassword= "Sakshi@9044"
+let accountState;
+//emailId=2 //not allowed
+/* const value never will be changed
+{} called scope
+ prefer not use var because of issue in functional block & block scope .
+*/
+
+accountId= "9876"
+accountCity= "jaipur"
+accountpassword ="swati@234"
+
+console.log(emailId)
+console.table([emailId, accountId, accountCity, accountpassword, accountState])
