4a53f441efab9d0eb106e444fb8527b536bb25c3
sakshi9324 <156990357+sakshi9324@users.noreply.github.com>
2024-12-16 07:39:52 +0000

    understanding the 02_dataTypes

---
+++ o1basics_Js/02_dataTypes.js
@@ -0,0 +1,29 @@
+/* NOTE :- Javascript engine hidden in browser  // alert (3+3) we are using node.js not browser  
+// EXTENSION :- Prettier (for code indent)
+// for  read about the ALERT we read the MDN document
+// Original document of Javascript :- https://tc39.es/ecma262/*/ 
+
+"use strict"; // treat all js code as newer version
+
+console.log(9
+    +9) // code readability should be high
+console.log("sakshi")
+
+let city;
+let number =9
+let name= "sakshi"
+let isLoggedIn= false
+let state= null // like no temprature value 
+
+//number :- 2 to power 53
+//bigint ex:- tradding, facebbok e.t.c
+//string
+//boolean = true/false
+//null(representation empty value) = standalone/value
+//symbol :- for identifying uniquely
+
+console.log(typeof 9);
+console.log(typeof null);// object
+console.log(typeof undefined);//undefined
+console.log(typeof false);
+console.log(typeof "sakshi");
