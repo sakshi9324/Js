221b0fe33f2dc65032b3d7a9212733e20b881b0c
sakshi9324 <156990357+sakshi9324@users.noreply.github.com>
2024-12-20 09:43:06 +0000

    about the conversion

---
+++ o1basics_Js/03_conversionOperation.js
@@ -0,0 +1,37 @@
+let age = 33
+console.log(typeof age);
+console.log(typeof(age));
+
+let a = "33abc"
+console.log(typeof(a));
+console.log(a)
+
+let score = true
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(typeof true);
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber =99
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+let sakshi = 89
+let value = Number(sakshi)
+console.log(value);
+console.log(typeof value);
+
+//"33" => 33
+// "33abc" => NaN 
+// 1=> true; 0=> false
+// "" => false
+// "histesh" => true
